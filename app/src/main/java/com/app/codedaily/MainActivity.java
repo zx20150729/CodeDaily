5a396462a176b7150d233e938d08a6f0903da3ba
zx20110729 <751945866@qq.com>
2016-11-10 17:31:59 +0800

    add a Toast

--- app/src/main/java/com/app/codedaily/MainActivity.java
+++ app/src/main/java/com/app/codedaily/MainActivity.java
@@ -1,13 +1,16 @@
 package com.app.codedaily;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"你能看见我吗?",Toast.LENGTH_SHORT).show();
     }
 }
